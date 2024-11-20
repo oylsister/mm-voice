85e1f3e2690a407e00a662dbb47697b7486cec81
Oylsister <kkuiba2009@gmail.com>
2024-11-21 01:15:31 +0700

    More progression

--- Voice.cpp
+++ Voice.cpp
@@ -15,6 +15,7 @@
 #include <stdio.h>
 #include "Voice.h"
 #include "iserver.h"
+#include "protobuf/generated/netmessages.pb.h"
 
 SH_DECL_HOOK3_void(IServerGameDLL, GameFrame, SH_NOATTRIB, 0, bool, bool, bool);
 SH_DECL_HOOK4_void(IServerGameClients, ClientActive, SH_NOATTRIB, 0, CPlayerSlot, bool, const char *, uint64);
@@ -34,6 +35,12 @@ IVEngineServer *engine = NULL;
 IGameEventManager2 *gameevents = NULL;
 IGameEventSystem* g_gameEventSystem = nullptr;
 ICvar *icvar = NULL;
+INetworkGameServer* g_pNetworkGameServer = nullptr;
+IVEngineServer2* g_pEngineServer2 = nullptr;
+
+int g_SectionNumber = 0;
+
+CServerSideClient* g_AudioPlayerClient = nullptr;
 
 // Should only be called within the active game loop (i e map should be loaded and active)
 // otherwise that'll be nullptr!
@@ -52,6 +59,7 @@ bool VoiceData::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, boo
 {
 	PLUGIN_SAVEVARS();
 
+	GET_V_IFACE_CURRENT(GetEngineFactory, g_pEngineServer2, IVEngineServer2, SOURCE2ENGINETOSERVER_INTERFACE_VERSION);
 	GET_V_IFACE_CURRENT(GetEngineFactory, engine, IVEngineServer, INTERFACEVERSION_VENGINESERVER);
 	GET_V_IFACE_CURRENT(GetEngineFactory, icvar, ICvar, CVAR_INTERFACE_VERSION);
 	GET_V_IFACE_ANY(GetServerFactory, server, IServerGameDLL, INTERFACEVERSION_SERVERGAMEDLL);
@@ -81,6 +89,8 @@ bool VoiceData::Load(PluginId id, ISmmAPI *ismm, char *error, size_t maxlen, boo
 	g_pCVar = icvar;
 	ConVar_Register( FCVAR_RELEASE | FCVAR_CLIENT_CAN_EXECUTE | FCVAR_GAMEDLL );
 
+	g_pNetworkGameServer = g_pNetworkServerService->GetIGameServer();
+
 	return true;
 }
 
@@ -95,6 +105,12 @@ bool VoiceData::Unload(char *error, size_t maxlen)
 	SH_REMOVE_HOOK(IServerGameClients, ClientConnect, gameclients, SH_MEMBER(this, &VoiceData::Hook_ClientConnect), false);
 	SH_REMOVE_HOOK(IServerGameClients, ClientCommand, gameclients, SH_MEMBER(this, &VoiceData::Hook_ClientCommand), false);
 
+	if (g_pNetworkGameServer)
+		delete g_pNetworkGameServer;
+
+	if (g_pEngineServer2)
+		delete g_pEngineServer2;
+
 	return true;
 }
 
@@ -103,6 +119,98 @@ void VoiceData::AllPluginsLoaded()
 	/* This is where we'd do stuff that relies on the mod or other plugins 
 	 * being initialized (for example, cvars added and events registered).
 	 */
+}
+
+CUtlVector<CServerSideClient*>* GetClientList()
+{
+	if (!g_pNetworkGameServer)
+		return nullptr;
+
+#ifdef PLATFORM_WINDOWS
+	static constexpr int offset = 78;
+#else
+	static constexpr int offset = 80;
+#endif
+	return (CUtlVector<CServerSideClient*> *)(&g_pNetworkGameServer[offset]);
+}
+
+CServerSideClient* GetClientBySlot(CPlayerSlot slot)
+{
+	CUtlVector<CServerSideClient*>* pClients = GetClientList();
+
+	if (!pClients)
+		return nullptr;
+
+	return pClients->Element(slot.Get());
+}
+
+CServerSideClient* GetFakeClient(const char* name)
+{
+	auto pClients = GetClientList();
+	CServerSideClient* fakeClient = nullptr;
+	for (int i = 0; i < pClients->Count(); i++)
+	{
+		if (pClients->IsValidIndex(i))
+		{
+			auto client = pClients->Element(i);
+			if (client->IsInGame() && (client->IsFakePlayer() || client->IsHLTV()))
+			{
+				fakeClient = client;
+				break;
+			}
+		}
+	}
+	if (!fakeClient)
+	{
+		CPlayerSlot slot = g_pEngineServer2->CreateFakeClient(name);
+		fakeClient = GetClientBySlot(slot);
+	}
+	return fakeClient;
+}
+
+void VoiceData::Init()
+{
+
+}
+
+void VoiceData::HandleVoiceData()
+{
+
+}
+
+void VoiceData::SendVoiceData()
+{
+	INetworkMessageInternal* pSVC_VoiceData = g_pNetworkMessages->FindNetworkMessageById(47);
+	CNetMessagePB<CSVCMsg_VoiceData>* pData = pSVC_VoiceData->AllocateMessage()->ToPB<CSVCMsg_VoiceData>();
+
+	g_AudioPlayerClient = GetFakeClient("Sympho Bot");
+
+	pData->set_xuid(0);
+	pData->set_client(g_AudioPlayerClient->GetPlayerSlot().Get());
+
+	CMsgVoiceAudio *audio = pData->mutable_audio();
+
+	audio->set_voice_data(0);
+	audio->set_format(VOICEDATA_FORMAT_ENGINE);
+	audio->set_sample_rate(24000);
+	audio->set_sequence_bytes(0);
+	audio->set_section_number(0);
+	audio->set_voice_level(1.0);
+	audio->set_uncompressed_sample_offset(0);
+
+	CUtlVector<CServerSideClient*>* client_list = GetClientList();
+	for (int i = 0; i < client_list->Count(); i++)
+	{
+		if (!client_list->IsValidIndex(i))
+		{
+			continue;
+		}
+		CServerSideClient* client = client_list->Element(i);
+		if (client->IsInGame() && !client->IsFakePlayer() && !client->IsHLTV())
+		{
+			client->GetNetChannel()->SendNetMessage(pData, NetChannelBufType_t::BUF_VOICE);
+		}
+	}
 }
 
 void VoiceData::Hook_ClientActive( CPlayerSlot slot, bool bLoadGame, const char *pszName, uint64 xuid )
@@ -144,12 +252,7 @@ void VoiceData::Hook_ClientDisconnect( CPlayerSlot slot, ENetworkDisconnectionRe
 
 void VoiceData::Hook_GameFrame( bool simulating, bool bFirstTick, bool bLastTick )
 {
-	/**
-	 * simulating:
-	 * ***********
-	 * true  | game is ticking
-	 * false | game is not ticking
-	 */
+	HandleVoiceData();
 }
 
 void VoiceData::OnLevelInit( char const *pMapName,
